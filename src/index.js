4a659272f3de22e071696dd6da82886809eeddcf
shefinannathampy <shefin04annathampy94@gmail.com>
2023-04-14 19:06:17 -0400

    miniProject

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector('#create-task-form');
+const taskList = document.querySelector('#tasks');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault(); 
+  const newTask = document.createElement('li');
+  const taskDescription = document.querySelector('#new-task-description');
+  newTask.innerText = taskDescription.value;
+
+  taskList.appendChild(newTask);
+
+  taskDescription.value = '';
+});
 });
